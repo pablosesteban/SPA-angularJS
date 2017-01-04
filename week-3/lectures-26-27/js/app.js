797bfb3bf4b3471ef6d37407a4a6415922444ec1
Santamarta Esteban, Pablo <pablo.santamarta-esteban@capgemini.com>
2017-01-04 14:15:15 +0100

    week-3

---
+++ week-3/lectures-26-27/js/app.js
@@ -0,0 +1,70 @@
+/*
+Creating CUSTOM DIRECTIVES
+*/
+(function() {
+    'use strict';
+
+    angular.module("MyApp", [])
+
+    .controller("MyCtrl", [function() {}])
+    /*
+    STEP 1: register the directive with angular module
+        The same way you register controllers, services, factories and so on
+
+        The first argument is the directive name which is a NORMALIZED name that will appear in HTML
+
+        The second argument is the Factory function that returns what's called a DDO (Directive Definition Object) which is basically a configuration object that tells angular compiler how this directive should behave when angular finds this tag or element or attribute in our HTML
+
+        The Factory function will execute ONLY ONCE and not everytime it finds our directive mentioned that now HTML, so since it's only going to get executed once you could use that function to perform any initializations you need besides obviously returning the special object DDO
+    */
+    .directive("myTag", MyTag)
+    .directive("myTagDiv", MyTagDiv);
+
+    /*
+    STEP 2: define our Factory Function
+        We can Inject other things
+
+        It has to return de DDO
+    */
+    function MyTag() {
+        /*
+        The DDO consists of multiple properties that are defined in the AngularJS documentation
+
+        The scope of your directive will be the same scope of the containing controller unless you specify otherwise, i.e. the $scope inside of our directive was the exact same object as the $scope of our directive's parent controller
+
+        That type of setup makes a directive dependent on our particular controller, i.e. there's TOO MUCH COUPLING going on between the controller and the directive
+        */
+        var ddo = {
+            //every time we use MyTag in our HTML the words Hello World will be displayed to the user
+            template: "Your Name is: {{name}}"
+        };
+
+        return ddo;
+    }
+
+    function MyTagDiv() {
+        var ddo = {
+            //to point to a HTML template or HTML file
+            templateUrl: "template/template.html",
+            /*
+            DDO restrict property:
+                Tells AngularJS compiler where to detect your custom directive
+
+                Default value is AE: "A" stands for attribute, and "E" stands for element that means that angular will look for your directive as being either an attribute or an element
+
+                The best practice to restrict your directive to an attribute (A) if that directive is extending some behavior of some other element
+
+                The best practice to restrict a directive to be an element if that directive is defining a component with some associated template
+
+                It's a best practice NOT to use the class on CSS-based directives
+
+                If you restrict a directive to be a one type and then you use that directive in a different fashion, Angular will not match that directive and will simply ignore it like anything else in your HTML then it's not supposed to process
+
+                The associated template that comes with their particular directive has to make sense within the context to the HTML
+            */
+            restrict: "AE"
+        };
+
+        return ddo;
+    }
+})();
---
+++ week-3/lectures-28/js/app.js
@@ -0,0 +1,152 @@
+(function () {
+'use strict';
+
+angular.module('ShoppingListDirectiveApp', [])
+.controller('ShoppingListController1', ShoppingListController1)
+.controller('ShoppingListController2', ShoppingListController2)
+.factory('ShoppingListFactory', ShoppingListFactory)
+
+.directive('shoppingList', ShoppingList);
+
+/*
+ISOLATE SCOPE:
+    It would be better to break away from this directive scope inheritance, and create an isolated scope for our directive
+
+    Breaks the prototypal inheritance of the scope from the parent
+
+    Makes the directive more independent, therefore less coupled with the controller, or whatever parent component the directive is living in
+*/
+function ShoppingList() {
+  var ddo = {
+    templateUrl: 'template/shoppingList.html',
+      /*
+      If it's isolated, how do we get the data that we need into our directive? -> Explicitly bind pre-defined attributes to the isolate scope properties
+      */
+    scope: {
+        /*
+        We're specifying a property on the scope object called "list"
+
+        The value is the HTML template attribute name
+
+        In our HTML template, we use the same normalization rules we use for the element name
+
+        Bidirectional Binding "=attributeName":
+            Is probably the most communicative way, means that if you change the parent value the value in the directive will change automatically and vice versa
+
+            If you leave the value as simply an equal sign (=), AngularJS compiler will assume that the name of the attribute in the HTML template is the same as your directives property name
+
+            If you follow the equals sign with a question mark (=?), that will signify to the Angular compiler that the name of the attribute in the HTML template is the same as your directives property name and it is optional
+
+        One Way Binding "@":
+            Tells the Angular compiler that we want to bind the property to the DOM's attribute "value" that's located in the same element as our directive
+
+            The result value that's assigned the property in our directive scope in this setup is always a STRING
+
+            The outer value (DOM's value attribute) change affects the inner value but if we explicitly change the property that's bound to the attribute in side of the directive the value of the attribute DOM node will be unaffected
+        */
+      list: '=myList',
+      title: '@title'
+    }
+  };
+
+  return ddo;
+}
+
+// LIST #1 - controller
+ShoppingListController1.$inject = ['ShoppingListFactory'];
+function ShoppingListController1(ShoppingListFactory) {
+  var list = this;
+
+  // Use factory to create new shopping list service
+  var shoppingList = ShoppingListFactory();
+
+  list.items = shoppingList.getItems();
+  var origTitle = "Shopping List #1";
+
+  list.title = origTitle + " (" + list.items.length + " items )";
+
+  list.itemName = "";
+  list.itemQuantity = "";
+
+  list.addItem = function () {
+    shoppingList.addItem(list.itemName, list.itemQuantity);
+
+      //CHANGE WHENEVER THE LENGHT CHANGES
+    list.title = origTitle + " (" + list.items.length + " items )";
+  };
+
+  list.removeItem = function (itemIndex) {
+    shoppingList.removeItem(itemIndex);
+
+      //CHANGE WHENEVER THE LENGHT CHANGES
+    list.title = origTitle + " (" + list.items.length + " items )";
+  };
+}
+
+
+// LIST #2 - controller
+ShoppingListController2.$inject = ['ShoppingListFactory'];
+function ShoppingListController2(ShoppingListFactory) {
+  var list = this;
+
+  // Use factory to create new shopping list service
+  var shoppingList = ShoppingListFactory(3);
+
+  list.items = shoppingList.getItems();
+
+  list.itemName = "";
+  list.itemQuantity = "";
+
+  list.addItem = function () {
+    try {
+      shoppingList.addItem(list.itemName, list.itemQuantity);
+    } catch (error) {
+      list.errorMessage = error.message;
+    }
+
+  };
+
+  list.removeItem = function (itemIndex) {
+    shoppingList.removeItem(itemIndex);
+  };
+}
+
+// If not specified, maxItems assumed unlimited
+function ShoppingListService(maxItems) {
+  var service = this;
+
+  // List of shopping items
+  var items = [];
+
+  service.addItem = function (itemName, quantity) {
+    if ((maxItems === undefined) ||
+        (maxItems !== undefined) && (items.length < maxItems)) {
+      var item = {
+        name: itemName,
+        quantity: quantity
+      };
+      items.push(item);
+    }
+    else {
+      throw new Error("Max items (" + maxItems + ") reached.");
+    }
+  };
+
+  service.removeItem = function (itemIndex) {
+    items.splice(itemIndex, 1);
+  };
+
+  service.getItems = function () {
+    return items;
+  };
+}
+
+function ShoppingListFactory() {
+  var factory = function (maxItems) {
+    return new ShoppingListService(maxItems);
+  };
+
+  return factory;
+}
+
+})();
